60a1ab4d81dc76131809cff7520a4031c8dc8d7c
Zonmgin-Zhang <85326183+Zonmgin-Zhang@users.noreply.github.com>
2023-04-28 19:28:02 +0800

    Update download.py

--- aliyunapi/download.py
+++ aliyunapi/download.py
@@ -62,17 +62,6 @@ def download_all():
     else:
         ali.download_folder(file.file_id, local_folder=local_folder)
         
-    ali = Aligo()
-    file = ali.get_file_by_path('1GB.txt')
-
-    local_folder = ''
-    if file.type == 'file':
-        start_time = time.time()
-        ali.download_file(file=file, local_folder=local_folder)
-        end_time = time.time() - start_time
-        print("1GB file's downloading duration time: ", end_time)
-    else:
-        ali.download_folder(file.file_id, local_folder=local_folder)
 
 
 
